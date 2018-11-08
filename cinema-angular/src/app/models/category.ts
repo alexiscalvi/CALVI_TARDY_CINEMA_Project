77be1060bd45bc1c39e716091b18da355fd9e360
Martial Tardy <tardy.m02@gmail.com>
2018-11-08 14:36:44 +0100

    create class category and complex-film

---
+++ cinema-angular/src/app/models/category.ts
@@ -0,0 +1,7 @@
+export class Category {
+  constructor(
+    public categoryId,
+    public name: String,
+    public lastUpdate
+  ){}
+}
---
+++ cinema-angular/src/app/models/complex-film.ts
@@ -0,0 +1,23 @@
+import {Actor} from './actor';
+import {Category} from './category';
+
+export class ComplexFilm {
+  constructor(
+    public filmId?,
+    public title?: String,
+    public description?: String,
+    public release_year?: String,
+    public language_id?: String,
+    public original_language_id?: String,
+    public rental_duration?: String,
+    public rental_rate?: String,
+    public length?: String,
+    public replacement_cost?: String,
+    public rating?: String,
+    public special_features?: String,
+    public last_update?: String,
+    public actors?: Actor[],
+    public categories?: Category[]
+  ) {
+  }
+}
